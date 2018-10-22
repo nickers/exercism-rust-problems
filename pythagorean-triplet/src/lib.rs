c4a52d1935cc802c92188d1f8ff7e266eec38ec4
Tomasz Wsuł <2nickers@gmail.com>
2018-10-22 23:54:55 +0200

    pythagorean-triplet - solution

--- pythagorean-triplet/src/lib.rs
+++ pythagorean-triplet/src/lib.rs
@@ -1,3 +1,11 @@
 pub fn find() -> Option<u32> {
-    unimplemented!();
+    for a in 1..998 {
+        for b in (a+1)..(999-a) {
+            let c = 1000 - a - b;
+            if (a*a + b*b) == c*c {
+                return Some(a * b * c)
+            }
+        }
+    }
+    return None
 }
