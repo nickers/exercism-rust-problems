aea22ac89bbf45dbf51169527c33ce2aa0bbe859
Tomasz Wsuł <2nickers@gmail.com>
2018-10-23 23:57:41 +0200

    gigasecond solved - more readable

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -3,5 +3,5 @@ use chrono::{DateTime, Utc, Duration};
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    start + Duration::seconds(1000000000)
+    start + Duration::seconds(10i64.pow(9))
 }
