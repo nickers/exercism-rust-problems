a7fe129068199c556aa9cbde4271a91dd688212d
Tomasz Wsuł <2nickers@gmail.com>
2018-11-09 23:15:55 +0100

    pythagorean-triplet - more idiomatic version

--- pythagorean-triplet/src/lib.rs
+++ pythagorean-triplet/src/lib.rs
@@ -1,11 +1,11 @@
 pub fn find() -> Option<u32> {
-    for a in 1..998 {
+    for a in 1u32..998 {
         for b in (a+1)..(999-a) {
             let c = 1000 - a - b;
-            if (a*a + b*b) == c*c {
+            if a.pow(2) + b.pow(2) == c.pow(2) {
                 return Some(a * b * c)
             }
         }
     }
-    return None
+    None
 }
