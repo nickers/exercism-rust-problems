81fdf3e55481def986fe6a1cf525c5021667eb56
Tomasz Wsuł <2nickers@gmail.com>
2019-02-17 21:25:22 +0100

    saddle-points - solved

--- saddle-points/src/lib.rs
+++ saddle-points/src/lib.rs
@@ -1,6 +1,31 @@
+use std::cmp;
+
 pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
-    unimplemented!(
-        "find the saddle points of the following matrix: {:?}",
-        input
-    )
+    let mut smallest :Vec<u64> = Vec::new();
+    for row in input {
+        for i in 0..smallest.len() {
+            smallest[i] = cmp::min(row[i], smallest[i])
+        }
+        if smallest.len() < row.len() {
+            let rg = smallest.len()..row.len();
+            smallest.extend_from_slice(&row.as_slice()[rg]);
+        }
+    }
+
+    let mut saddles : Vec<(usize, usize)> = Vec::new();
+
+    for (y, row) in input.iter().enumerate() {
+        match row.iter().max() {
+            None => {},
+            Some (m) => {
+                for (x, v) in row.iter().enumerate() {
+                    if *v == smallest[x] && v == m {
+                        saddles.push((y, x))
+                    }
+                }
+            },
+        }
+    }
+
+    saddles
 }
