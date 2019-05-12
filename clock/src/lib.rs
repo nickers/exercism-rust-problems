0f4d0dace0f911306cb2ded3495b219e4f610814
Tomasz Wsuł <2nickers@gmail.com>
2019-05-12 18:46:45 +0200

    clock - formatted with `cargo fmt`

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,28 +1,32 @@
 use std::fmt;
 
-const CLOCK_MAX_MINUTES: i32 = 24*60;
+const CLOCK_MAX_MINUTES: i32 = 24 * 60;
 
 pub struct Clock {
-    total_minutes: i32
+    total_minutes: i32,
 }
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        Clock{
-            total_minutes: 0
-        }.add_minutes(hours * 60 + minutes)
+        Clock { total_minutes: 0 }.add_minutes(hours * 60 + minutes)
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        Clock { 
-            total_minutes: (self.total_minutes + (minutes % CLOCK_MAX_MINUTES) + CLOCK_MAX_MINUTES ) % CLOCK_MAX_MINUTES
+        Clock {
+            total_minutes: (self.total_minutes + (minutes % CLOCK_MAX_MINUTES) + CLOCK_MAX_MINUTES)
+                % CLOCK_MAX_MINUTES,
         }
     }
 }
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:02}:{:02}", self.total_minutes / 60, self.total_minutes % 60)
+        write!(
+            f,
+            "{:02}:{:02}",
+            self.total_minutes / 60,
+            self.total_minutes % 60
+        )
     }
 }
 
