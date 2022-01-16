8a70273c35079adb98f7cf7a910451bdb48adc35
Tomasz Wsuł <2nickers@gmail.com>
2022-01-17 00:34:08 +0100

    dot-dsl - compiling

--- dot-dsl/src/lib.rs
+++ dot-dsl/src/lib.rs
@@ -1,9 +1,76 @@
 pub mod graph {
-    pub struct Graph;
+
+    use std::collections::HashMap;
+
+    pub mod graph_items {
+        pub mod node {
+            #[derive(PartialEq)]
+            #[derive(Debug)]
+            pub struct Node;
+        }
+        pub mod edge {
+            #[derive(PartialEq)]
+            #[derive(Debug)]
+            pub struct Edge;
+        }
+    }
+
+    #[derive(Debug)]
+    pub struct Graph {
+        pub edges: Vec<graph_items::edge::Edge>,
+        pub nodes: Vec<graph_items::node::Node>,
+        pub attrs: HashMap<String, String>
+    }
 
     impl Graph {
         pub fn new() -> Self {
-            unimplemented!("Construct a new Graph struct.");
+            Self {
+                edges: Vec::new(),
+                nodes: Vec::new(),
+                attrs: HashMap::new()
+            }
+            // unimplemented!("Construct a new Graph struct.");
+        }
+
+        pub fn with_nodes(&self, _nodes: &Vec<graph_items::node::Node>) -> Self {
+            unimplemented!("x")
+        }
+
+        pub fn with_edges(&self, _edges: &Vec<graph_items::edge::Edge>) -> Self {
+            unimplemented!("y")
+        }
+
+        pub fn with_attrs(&self, _attrs: &[(&str, &str)]) -> Self {
+            unimplemented!("s")
+        }
+
+        pub fn get_node(&self, _name: &str) -> Result<graph_items::node::Node, &str> {
+            unimplemented!("")
         }
     }
+
+    impl graph_items::edge::Edge {
+        pub fn new(_from: &str, _to: &str) -> Self {
+            unimplemented!("node");
+        }
+
+        pub fn with_attrs(&self, _attrs: &[(&str, &str)]) -> Self {
+            unimplemented!("s")
+        }
+    }
+
+    impl graph_items::node::Node {
+        pub fn new(_name: &str) -> Self {
+            unimplemented!("node");
+        }
+
+        pub fn with_attrs(&self, _attrs: &[(&str, &str)]) -> Self {
+            unimplemented!("s")
+        }
+
+        pub fn get_attr(&self, _name: &str) -> Option<&str> {
+            unimplemented!("node::get_attr")
+        }
+    }
+
 }
